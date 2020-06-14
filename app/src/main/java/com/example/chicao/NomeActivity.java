1f745733a9a4cc1eaf396bc7c71d591372d3fb81
chicaoapp <66920957+chicaoapp@users.noreply.github.com>
2020-06-14 19:27:37 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/chicao/NomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.chicao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nome);
+    }
+}
